8d2926f2c3aa48104cba5a23a8cae59a0b7f5406
KyleClight <belkius102@gmail.com>
2026-03-18 03:20:56 +0600

    initial commit

---
+++ src/main/java/com/belkius/loonie/LoonieApplication.java
@@ -0,0 +1,13 @@
+package com.belkius.loonie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoonieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoonieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/belkius/loonie/LoonieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belkius.loonie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoonieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
