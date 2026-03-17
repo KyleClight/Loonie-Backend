e56565db40c44c49715230985d044eab1bcb7408
KyleClight <belkius102@gmail.com>
2026-03-18 03:47:50 +0600

    feat: added user repository layer

--- src/main/java/com/belkius/loonie/Repository/loonieRepository.java
+++ src/main/java/com/belkius/loonie/Repository/loonieRepository.java
@@ -1,5 +1,15 @@
 package com.belkius.loonie.Repository;
 
-public interface loonieRepository {
+import com.belkius.loonie.Entity.userEntity;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface loonieRepository extends JpaRepository<userEntity, Integer>{
+    Optional<userEntity> findByEmail(String email);
+    
+    Boolean existsByUserId(int userId);
 }
